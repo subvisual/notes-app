d7fb6f2173348f4f6daa3d534472aa5ab1fc85c9
Paula Carneiro <paulacarneiro215@gmail.com>
2022-08-09 18:51:59 +0100

    Replace ethereum provider type

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
-import { MetaMaskInpageProvider } from '@metamask/providers';
+import { ExternalProvider } from '@ethersproject/providers';
 
 declare global {
   interface Window {
-    ethereum?: MetaMaskInpageProvider;
+    ethereum?: ExternalProvider;
   }
 }
