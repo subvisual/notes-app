eea15bcd970f4eda6ffd208ddeb6587b9408b5e2
Paula Carneiro <paulacarneiro215@gmail.com>
2022-08-26 14:02:50 +0100

    Break down menu into smaller components

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,10 +1,9 @@
-import { useState, useEffect, MouseEvent } from "react";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import FoldersList from "./folders-list";
+import { useState, useEffect } from "react";
 import { useStore } from "../lib/store";
-import TagsList from "./tags-list";
 import SearchBar from "./search-bar";
 import Logo from "./logo";
+import MenuTabs from "./menu-tabs";
+import MenuActions from "./menu-actions";
 import Modal, { ModalActions } from "./modal";
 
 export default function Menu() {
@@ -20,9 +19,6 @@ export default function Menu() {
     setTags();
   }, [allNotes, setTags]);
 
-  const changeModalAction = (ev: MouseEvent) =>
-    setModalAction((ev.target as HTMLButtonElement).value as ModalActions);
-
   const closeModal = () => setModalAction(ModalActions.NONE);
 
   return (
@@ -32,55 +28,8 @@ export default function Menu() {
           <Logo width="120" length="45" />
           <SearchBar />
         </div>
-        <Tabs className="w-full grow">
-          <TabList className="grid h-12 w-full grid-cols-[1fr_1fr] border-b-thin border-dark-4 text-center dark:border-light-4">
-            <Tab
-              className="flex w-full items-center justify-center"
-              selectedClassName="rounded-tr-lg dark:bg-dark-2 bg-light-3 border-t-thin border-r-thin border-dark-4 dark:border-light-4"
-            >
-              Folders
-            </Tab>
-            <Tab
-              className="flex w-full items-center justify-center"
-              selectedClassName="rounded-tl-lg dark:bg-dark-2 bg-light-3 border-t-thin border-l-thin border-dark-4 dark:border-light-4"
-            >
-              Tags
-            </Tab>
-          </TabList>
-
-          <TabPanel>
-            <FoldersList />
-          </TabPanel>
-          <TabPanel>
-            <TagsList />
-          </TabPanel>
-        </Tabs>
-        <div className="flex h-28 flex-col text-xs">
-          <button
-            type="button"
-            className="p-2"
-            onClick={changeModalAction}
-            value={ModalActions.ADD_NOTE}
-          >
-            ADD NOTE
-          </button>
-          <button
-            type="button"
-            className="p-2"
-            onClick={changeModalAction}
-            value={ModalActions.ADD_FOLDER}
-          >
-            ADD FOLDER
-          </button>
-          <button
-            type="button"
-            className="p-2"
-            onClick={changeModalAction}
-            value={ModalActions.DELETE_FOLDER}
-          >
-            DELETE FOLDER
-          </button>
-        </div>
+        <MenuTabs />
+        <MenuActions setModalAction={setModalAction} />
       </div>
       <Modal type={modalAction} closeModal={closeModal} />
     </>
