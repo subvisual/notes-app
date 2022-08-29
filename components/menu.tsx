8f5d7a8ba707b23755874a1c31a08fc80fbd3ff4
Paula Carneiro <paulacarneiro215@gmail.com>
2022-08-29 19:05:24 +0100

    Add theme button

--- components/menu.tsx
+++ components/menu.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useStore } from "../lib/store";
 import SearchBar from "./search-bar";
 import Logo from "./logo";
+import ThemeButton from "./theme-button";
 import MenuTabs from "./menu-tabs";
 import MenuActions from "./menu-actions";
 import Modal, { ModalActions } from "./modal";
@@ -25,7 +26,10 @@ export default function Menu() {
     <>
       <div className="text-md z-10 grid h-screen w-full grid-rows-[7.8rem,_1fr,_5.8rem] bg-light-2 shadow-[0_0px_15px_rgba(0,0,0,.3)] dark:bg-dark-3">
         <div className="h-full w-full p-5">
-          <Logo width="120" length="45" />
+          <div className="flex items-center">
+            <Logo width="120" length="45" />
+            <ThemeButton />
+          </div>
           <SearchBar />
         </div>
         <MenuTabs />
---
+++ components/theme-button.tsx
@@ -0,0 +1,25 @@
+import { useStore, Theme } from "../lib/store";
+import SunSVG from "../assets/sun.svg";
+import MoonSVG from "../assets/moon.svg";
+
+export default function ThemeButton() {
+  const {
+    preferences: { theme, setTheme },
+  } = useStore();
+
+  const toggleTheme = () => {
+    setTheme(theme === Theme.Dark ? Theme.Light : Theme.Dark);
+  };
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="h-8 w-8 overflow-hidden rounded-md bg-light-1 drop-shadow-connect-light  dark:bg-dark-1 dark:shadow-none"
+    >
+      <span className="flex -translate-y-7 flex-col items-center gap-2 transition-all ease-out dark:translate-y-1">
+        <MoonSVG className="h-6 w-6 stroke-light-3 stroke-2" />
+        <SunSVG className="h-6 w-6 stroke-dark-3 stroke-1" />
+      </span>
+    </button>
+  );
+}
