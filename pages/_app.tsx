ef1746b69b6b5c8802c4ed95fbb0b6e15081ef98
Paula Carneiro <paulacarneiro215@gmail.com>
2022-08-30 17:19:58 +0100

    Add about page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import "../styles/globals.css";
+import Head from "next/head";
 import type { AppProps } from "next/app";
+import "../styles/globals.css";
+import { useStore, Theme } from "../lib/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const {
+    preferences: { theme },
+  } = useStore();
+
+  return (
+    <div
+      className={`h-screen w-screen antialiased ${
+        theme === Theme.Dark ? "dark" : "light"
+      }`}
+    >
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="keywords"
+          content="finiam, notes, encryption, secure, metamask"
+        />
+        <meta name="description" content="secure notes app" />
+        <meta name="og:description" content="secure notes app" />
+        <meta name="twitter:description" content="secure notes app" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
